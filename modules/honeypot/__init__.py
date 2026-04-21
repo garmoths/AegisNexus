1fc96085025d087ef3141b741549c222535d25d0
garmoths <229919801+garmoths@users.noreply.github.com>
2026-04-21 09:18:40 +0300

    refactor: Honeypot modülünü IOC collector olarak güncelle

--- modules/honeypot/__init__.py
+++ modules/honeypot/__init__.py
@@ -1,7 +1,8 @@
 """
-02 - Honeypot Module (IP Avcısı)
-Dolandırıcıları tersine mühendislik ile avlayan tuzak modülü
+02 - Honeypot Module & IOC Collector
+IP Avcısı ve Indicator of Compromise (IoC) toplayıcı modülü
 """
 from .router import router
+from .ioc_api import router as ioc_router
 
-__all__ = ["router"]
+__all__ = ["router", "ioc_router"]
